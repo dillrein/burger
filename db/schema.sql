fab36541eb06a44a73ffecfac38d3984083ce968
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-09-26 14:23:41 -0700

    connection and db set up

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT AUTO_INCREREMENT NOT NULL,
+    burger_name VARCHAR (250) NOT NULL;
+    devoured BOOLEAN 
+)
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+    ("Cheese burger", "false")
+    ("ham burger", "false")
+    ("salad", "true"),
+    
